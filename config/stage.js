b26a11e3f2eb7b5504c017fae34842a08133d10c
Vitaliy Gnidenko <i@lart.com.ua>
2018-03-01 13:26:56 +0200

    use variables for ws host and ws port from environment

--- config/stage.js
+++ config/stage.js
@@ -8,7 +8,7 @@ module.exports = {
         }
     },
     ws:{
-        host: 'localhost',
-        port: 8080
+        host: '',
+        port: 0
     }
 }
--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const ws_server = require('./controllers/ws_server');
 const Offerings = require('./controllers/offerings');
 
 const server = new ws_server({
-        port: config.ws.port,
-        host: config.ws.host
+        port: config.ws.port || process.env.WS_PORT,
+        host: config.ws.host || process.env.WS_HOST
     });
 
 const offerings = new Offerings(server);
